6905f328cd1b5ccb93c72db70952e68f65f2208d
Adrian Moore <adrianrob@hotmail.com>
2023-01-13 09:24:01 -0800

    File/folder item is now a button.  Add declarative mini menu to each file/folder item.  Support for button color and disabling.

--- src/three/ng3-file-list/ng3-file-list.component.ts
+++ src/three/ng3-file-list/ng3-file-list.component.ts
@@ -6,18 +6,20 @@ import { OneDriveService } from '../../OneDrive/onedrive.service';
 import { FileData, FilterData } from '../../OneDrive/file-list';
 import { FlatUIInputService, FlatUIList, InteractiveObjects, ListItem, MenuItem } from 'ng3-flat-ui';
 import { NgtObjectProps } from '@angular-three/core';
-import { Group } from 'three';
+import { Group, MeshBasicMaterial } from 'three';
 
 @Component({
   selector: 'ng3-file-list',
   templateUrl: './ng3-file-list.component.html',
-  //changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [FlatUIInputService],
 })
 export class Ng3FileListComponent extends NgtObjectProps<Group> {
   @Input() selectable?: InteractiveObjects;
 
-  @ViewChild(FlatUIList) uilist!: FlatUIList;
+  //@ViewChild(FlatUIList) uilist!: FlatUIList;
+
+  protected rowheight = 0.2;
 
   protected filtereditems: Array<ListItem> = [];
   protected filtervalue = 'All Files';
@@ -27,9 +29,9 @@ export class Ng3FileListComponent extends NgtObjectProps<Group> {
 
   menuitems: Array<MenuItem> = [
     { text: 'Back', keycode: 'Backspace', icon: 'arrow_back', enabled: false, selected: () => { this.back() } },
-    { text: 'Create Folder', keycode: 'F2', icon: 'create_new_folder', enabled: true, selected: () => { this.createFolder(); } },
+    { text: 'Create Folder', keycode: '', icon: 'create_new_folder', enabled: true, color: new MeshBasicMaterial({ color: 'yellow'}), selected: () => { this.createFolder(); } },
     { text: 'Create File', keycode: 'Ctrl+N', icon: 'note_add', enabled: true, selected: () => { this.createFile(); } },
-    { text: 'Update File', keycode: 'Ctrl+S', icon: 'save', enabled: this.downloadUrl == undefined, selected: () => { this.updateFile(); } },
+    { text: 'Update File', keycode: 'Ctrl+S', icon: 'save', enabled: true, selected: () => { this.updateFile(); } },
     { text: 'Refresh', keycode: 'F5', icon: 'refresh', enabled: true, selected: () => { this.refresh(); } },
   ]
   protected menuwidth = 0;
@@ -101,8 +103,6 @@ export class Ng3FileListComponent extends NgtObjectProps<Group> {
 
   protected async refresh() {
     await this.getFiles(this.folderid);
-    this.cd.detectChanges();
-    this.uilist.movefirst();
   }
 
   private async getFiles(id?: string) {
@@ -121,16 +121,24 @@ export class Ng3FileListComponent extends NgtObjectProps<Group> {
 
   protected async open(item: FileData) {
     if (!item.id) return;
+
+    const back = this.menuitems[0];
+    const updatefile = this.menuitems[3];
+
     if (item.isfolder) {
       this.folders.push(this.folderid);
+      back.enabled = true;
+
       await this.getFiles(item.id);
       this.folderid = item.id;
       this.fileid = this.downloadUrl = undefined;
+      updatefile.enabled = false;
     }
     else {
       await this.graph.getDownloadUrl(item.id).then(data => {
         this.downloadUrl = data;
         this.fileid = item.id;
+        updatefile.enabled = true;
       });
     }
   }
@@ -139,6 +147,9 @@ export class Ng3FileListComponent extends NgtObjectProps<Group> {
     this.fileid = this.downloadUrl = undefined;
     this.folderid = this.folders.pop();
     await this.getFiles(this.folderid);
+
+    const back = this.menuitems[0];
+    back.enabled = this.folders.length > 0;
   }
 
   protected async createFolder() {
